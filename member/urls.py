3a0a068e1c120eeb97038179ff42a55eac761c3f
Jayanta <bjayanta.me@gmail.com>
2022-03-09 16:39:03 +0600

    work with forward & reverse relationship

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,9 @@
 from unicodedata import name
 from django.urls import path
-from .views import Home
+from .views import Home, Incity, Ininterest
 
 urlpatterns = [
     path('', Home.as_view(), name="member.home"),
+    path('<int:id>/incity/', Incity.as_view(), name="member.incity"),
+    path('<int:id>/ininterest/', Ininterest.as_view(), name="member.ininterest"),
 ]
--- member/views.py
+++ member/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import render, HttpResponse 
+from django.views.generic import View 
 from .models import *
 
 # Create your views here.
@@ -10,6 +10,35 @@ class Home(View):
         person = Person.objects.get(id=3)
 
         context['person'] = person
-        context['interests'] = person.interests.all()
+        context['interests'] = person.interests.all() # forward relationship 
+        context['address'] = person.personaddress
+        context['all_interests'] = Interest.objects.all()
+
+        city = City.objects.all()
+        context['cities'] = city
 
         return render(request, "member/home.html", context)
+
+class Incity(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        id = kwargs.get('id', 1)
+
+        city = City.objects.get(id=id)
+
+        context['city'] = city
+        context['all_person_address'] = city.personaddress_set.all() # reverse relationship 
+
+        return render(request, "member/incity.html", context)
+
+class Ininterest(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        id = kwargs.get('id', 1)
+
+        interest = Interest.objects.get(id=id)
+
+        context['interest'] = interest
+        context['all_person'] = interest.person_set.all() # reverse relationship 
+
+        return render(request, "member/ininterest.html", context)
